ed6504b3924e4347c89b7d14fc89093736b62a76
Kaito Udagawa <umireon@gmail.com>
2023-11-09 22:09:50 +0900

    Add pages

---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+	import { base } from '$app/paths';
+</script>
+
+<svelte:head>
+	<title>Recoreon</title>
+	<meta
+		name="description"
+		content="The evolutional screen recorder for streamers!"
+	/>
+</svelte:head>
+
+<section>
+	<header>
+		<h1>Recoreon</h1>
+		<h2>
+			The evolutional screen recorder for streamers!
+		</h2>
+	</header>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+</style>
